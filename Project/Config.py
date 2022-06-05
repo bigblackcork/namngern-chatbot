b66ea6d29b731af61abd50467a8e230ed8e97e2d
bigblackcork <manusak42@gmail.com>
2022-06-05 13:20:07 +0700

    chatbot

---
+++ Project/Config.py
@@ -0,0 +1,11 @@
+
+
+
+
+Channel_secret = "caadf9bd28b0c46e8b6a6558cbd31b86"
+
+
+Channel_access_token = "4yC+Q+HuPAtIHmKSL6KjC6yA9tOiVQ5gFd8Bqhy00A+riGpkUNg/PbVwVvKs/7KABxJBQ3CmgWqB5+HsMEjSTWt6fjD4LWQ4vQtF5lLH1Ogn+5/75o7mARBRZEneMsOqDy+fNeH2+H8qzuA8O9dZXAdB04t89/1O/w1cDnyilFU="
+
+
+Basic_ID = "@154kjnan"
